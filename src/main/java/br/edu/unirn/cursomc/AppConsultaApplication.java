69ef8375d7eae9fc8dadb7f41f22900220ae3d61
Felipe Lima <fearlimasi@gmail.com>
2018-02-16 20:16:44 -0300

    Criação do Projeto

---
+++ src/main/java/br/edu/unirn/cursomc/AppConsultaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.unirn.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConsultaApplication.class, args);
+	}
+}
